adcecf498f8cc44138ec596e5cbf12586c5056c2
Andrej Jokić <aj.jokic@gmail.com>
2020-09-29 08:44:19 +0200

    #PIT3-45 Created endpoints for liking/disliking movies

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Like;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Movie;
+use phpDocumentor\Reflection\DocBlock\Tags\Author;
 
 class MovieController extends Controller
 {
@@ -71,5 +74,33 @@ class MovieController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function like(Movie $movie)
+    {
+        $this->setLike($movie, auth()->user(), 1);
+        return $movie;
+    }
+
+    public function dislike(Movie $movie)
+    {
+        $this->setLike($movie, auth()->user(), -1);
+        return $movie;
+    }
+
+    public function unlike(Movie $movie)
+    {
+        $this->setLike($movie, auth()->user(), 0);
+        return $movie;
+    }
+
+    private function setLike(Movie $movie, User $user, $value) {
+        $like = Like::where('user_id', $user->id)->where('movie_id', $movie->id)->first();
+        if (!$like) $like = Like::make([
+            'user_id' => $user->id,
+            'movie_id' => $movie->id
+        ]);
+        $like->value = $value;
+        $like->save();
     }
 }
--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    protected $appends = ['likes', 'dislikes'];
+
+    public function getDislikesAttribute()
+    {
+        return $this->liked_by()->where('value', '=', -1)->count();
+    }
+
+    public function getLikesAttribute()
+    {
+        return $this->liked_by()->where('value', '=', 1)->count();
+    }
+
+    public function liked_by()
+    {
+        return $this->belongsToMany(User::class, 'likes')
+            ->using(Like::class)
+            ->withPivot([
+                'value'
+            ]);
+    }
 }
